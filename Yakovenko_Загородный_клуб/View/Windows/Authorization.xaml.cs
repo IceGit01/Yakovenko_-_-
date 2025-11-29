cd4142aab4b72dc0160ed2b8f5c3a7000247c437
Frosty_Yarik <151192679+IceGit01@users.noreply.github.com>
2025-11-29 23:10:27 +0300

    Добавьте файлы проекта.

---
+++ Yakovenko_Загородный_клуб/View/Windows/Authorization.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yakovenko_Загородный_клуб.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void NoAccountBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Registration createAccount = new Registration();
+            createAccount.ShowDialog();
+            this.Close();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ShowDialog();
+            this.Close();
+        }
+    }
+}
